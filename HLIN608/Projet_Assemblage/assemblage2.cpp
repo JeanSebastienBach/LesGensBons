7d59e8ce28552f9970552549083c358131bac2aa
sentes <antoine.afflatet@gmail.com>
2020-05-15 18:06:55 +0200

    assemblage correction

--- HLIN608/Projet_Assemblage/assemblage2.cpp
+++ HLIN608/Projet_Assemblage/assemblage2.cpp
@@ -19,6 +19,74 @@ Pour que m1 puisse être inclus dans m2 sans être un overlap : size(m1)<=size(m
 
 using namespace std;
 
+// Afficher un tableau
+void afficherTab(vector<string> T){
+	for(int i=0; i<=T.size(); i++){
+		if(i==0){
+			cout<<endl<<"[ ";
+		}
+		if(i==T.size()){
+			cout<<"]"<<endl;
+		}
+		else{
+			cout<<T[i]<<" ";
+		}
+	}
+}
+
+// Ajouter un élement en tête d'un tableau
+vector<string> ajouterTete(string x, vector<string> T){
+	vector<string> resultT;
+	resultT.push_back(x);
+	for(int i=0; i<T.size(); i++){
+		resultT.push_back(T[i]);
+	}
+	return resultT;
+}
+
+// Tableau sans sa tête
+vector<string> sansTete(vector<string> T){
+	vector<string> resultT;
+	for(int i=1; i<T.size(); i++){
+		resultT.push_back(T[i]);
+	}
+	return resultT;
+}
+
+// Fusionner 2 tableaux triés
+vector<string> fusion(vector<string> T1, vector<string> T2){
+	if(T1.size()==0){
+		return T2;
+	}
+	else if(T2.size()==0){
+		return T1;
+	}
+	else if(T1[0].size()<=T2[0].size()){
+		return ajouterTete(T1[0], fusion(sansTete(T1),T2));
+	}
+	else{
+		return ajouterTete(T2[0], fusion(T1,sansTete(T2)));
+	}
+}
+
+// Classer les mots selon leur longueur (tri fusion)
+vector<string> triMotsLongueur(int nbMots, vector<string> T){
+	if(nbMots<=1){
+		return T;
+	}
+
+	vector<string> T1,T2;
+	for(int i=0; i<nbMots; i++){
+		if(i<=(nbMots/2)-1){
+			T1.push_back(T[i]);
+		}
+		else{
+			T2.push_back(T[i]);
+		}
+	}
+	return fusion(triMotsLongueur(T1.size(),T1),triMotsLongueur(T2.size(),T2));
+}
+
 // Longueur maximale parmis les mots
 int longueurMax(int nbMots, vector<string> T){
 	int l=0;
@@ -28,6 +96,72 @@ int longueurMax(int nbMots, vector<string> T){
 		}
 	}
 	return l;
+}
+
+// Récupérer la liste des mots donnés en ligne de commande
+vector<string> recuperer_mots(int nbMots, char *argv[]){
+	vector<string> T;
+	for(int i=1; i<=nbMots; i++){
+		T.push_back(argv[i]);
+	}
+	return T;
+}
+
+// Afficher la liste des mots donnés en entrée
+void afficher_mots(int nbMots, vector<string> T, string description){
+	cout<<"LISTE DES "<<nbMots<<" MOTS "<<description<<" :"<<endl;
+	for(int i=0; i<nbMots; i++){
+		cout<<"   "<<i+1<<") "<<T[i]<<endl;
+	}
+}
+
+// Le mot x est sous-mot de y
+bool estSousMot(string x, string y){
+	
+	// S'il font la même taille
+	if(x.size()==y.size()){
+		return x==y;
+	}
+
+	/*if(x.size()==1){
+		for(int i=0; i<y.size(); i++){
+			if(x[0]==y[i])
+				return true;
+		}
+		return false;
+	}*/
+
+	bool result=false;
+	int diff=y.size()-x.size();
+	int i=0;
+	while(!result && i<=diff){
+		result = (x==y.substr(i,x.size()));
+		i++;
+	}
+
+	return result;
+}
+
+// Suppression des mots inclus dans d'autres
+vector<string> suppressionMotsInclus(int nbMots,vector<string> T){
+	vector<int> S; // Liste des indices des sous-mots
+	for(int i=0; i<nbMots; i++){
+		bool trouve=false;
+		for(int j=i+1; j<nbMots; j++){
+			if(estSousMot(T[i],T[j]) && !trouve){
+				S.push_back(i);
+				cout<<T[i]<<" ("<<i+1<<") est inclus dans "<<T[j]<<" ("<<j+1<<")"<<endl;
+				trouve=true;
+			}
+		}
+	}
+	cout<<endl;
+
+	// Suppression des doublons remarqués
+	for(int i=S.size()-1; i>=0; i--){
+		T.erase(T.begin()+ S[i]);
+	}
+	return T;
 }
 
 // Afficher la liste des mots donnés en entrée
@@ -41,28 +175,6 @@ vector<string> afficher_mots(int nbMots, char *argv[]){
 	return T;
 }
 
-// Le mot x est sous-mot de y
-bool estSousMot(string x, string y){
-	if(x.size()<=y.size()-2){
-		return true; // Temporaire
-	}
-	else{
-		return false;
-	}
-}
-
-// Suppression des mots inclus dans d'autres
-vector<string> suppressionMotsInclus(int nbMots,vector<string> T){
-	vector<string> S; // Liste des sous-mots
-	for(int i=0; i<nbMots; i++){
-		for(int j=0; j<nbMots; j++){
-			if(i!=j && estSousMot(T[i],T[j])){
-				S.push_back(T[i]);
-			}
-		}
-	}
-	return T; // Temporaire
-}
 
 // Calculer l'overlap entre deux mots 
 int calculerOverlap(string x, string y){
@@ -192,23 +304,43 @@ int main(int argc, char *argv[]){
 	}
 	else{
 
+
 		int nbMots = argc-1;
-
-		// Afficher la liste des mots donnés en entrée
-		cout<<endl;
-		vector<string> T = afficher_mots(nbMots, argv);
 		string motFusion = "";
+
+		// Récupérer et afficher la liste des mots donnés en entrée
+		cout<<endl;
+		vector<string> T = recuperer_mots(nbMots, argv);
+		afficher_mots(nbMots, T, "RÉCUPÉRÉS");
 		cout<<endl;
 
-		// Vérifier si certains mots sont inclus dans d'autres
+		// Trier le tableau selon la longueur des mots et afficher ces mots ainsi ordonnés
+		cout<<endl;
+		T = triMotsLongueur(nbMots, T);
+		afficher_mots(nbMots, T, "TRIÉS PAR LONGUEUR");
+		cout<<endl;
+
+		// Vérifier si certains mots sont inclus dans d'autres et afficher les mots sans ces mots inclus
+		cout<<endl;
 		T = suppressionMotsInclus(nbMots,T);
-		while(nbMots>1){
+		nbMots=T.size();
+		afficher_mots(nbMots, T, "SANS LES MOTS INCLUS");
+		cout<<endl;
 
-			int ecartAffichage = longueurMax(nbMots,T)+3;
+		if(nbMots==1){
+			motFusion = T[0];
+		}else{
 
-			// Parcourir tous les mots, calculer et afficher les overlaps
 			vector< vector<int> > M(nbMots, vector<int>(nbMots));
 			M = calculerOverlapM(T,M,nbMots);
+			int ecartAffichage;
+		}
+		while(nbMots>1){
+
+			ecartAffichage = longueurMax(nbMots,T)+3;
+
+			// Parcourir tous les mots, calculer et afficher les overlaps
+			
 			int i,j;
 			int overlapMaximum = overlapMax(M,nbMots,&i,&j);
 			afficherMatrice(T,M,nbMots,ecartAffichage,overlapMaximum,i,j);
