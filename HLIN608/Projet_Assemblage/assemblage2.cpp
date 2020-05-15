b8bf1bc06f51b7dc93ba93efe77a899863d9818f
sentes <antoine.afflatet@gmail.com>
2020-05-15 17:36:46 +0200

    ajout d'assemblage2.cpp

--- HLIN608/Projet_Assemblage/assemblage2.cpp
+++ HLIN608/Projet_Assemblage/assemblage2.cpp
@@ -6,8 +6,9 @@ HLIN608 - PROJET ALGO DU TEXTE - ALGORITHME D'ASSEMBLAGE :
 - Jérémie ROUX      
 (L3 GROUPE C)  
 
-Algorithme 2 :
-
+Algorithme 1 :
+Amélioration de Algorithme 0 en tenant compte des mots inclus dans les autres
+Pour que m1 puisse être inclus dans m2 sans être un overlap : size(m1)<=size(m2)-2
 */
 
 #include <iostream>
@@ -18,6 +19,16 @@ Algorithme 2 :
 
 using namespace std;
 
+// Longueur maximale parmis les mots
+int longueurMax(int nbMots, vector<string> T){
+	int l=0;
+	for(int i=0; i<nbMots; i++){
+		if(l<T[i].size()){
+			l=T[i].size();
+		}
+	}
+	return l;
+}
 
 // Afficher la liste des mots donnés en entrée
 vector<string> afficher_mots(int nbMots, char *argv[]){
@@ -28,6 +39,29 @@ vector<string> afficher_mots(int nbMots, char *argv[]){
 		cout<<"   "<<i<<") "<<T[i-1]<<endl;
 	}
 	return T;
+}
+
+// Le mot x est sous-mot de y
+bool estSousMot(string x, string y){
+	if(x.size()<=y.size()-2){
+		return true; // Temporaire
+	}
+	else{
+		return false;
+	}
+}
+
+// Suppression des mots inclus dans d'autres
+vector<string> suppressionMotsInclus(int nbMots,vector<string> T){
+	vector<string> S; // Liste des sous-mots
+	for(int i=0; i<nbMots; i++){
+		for(int j=0; j<nbMots; j++){
+			if(i!=j && estSousMot(T[i],T[j])){
+				S.push_back(T[i]);
+			}
+		}
+	}
+	return T; // Temporaire
 }
 
 // Calculer l'overlap entre deux mots 
@@ -62,6 +96,78 @@ int calculerOverlap(string x, string y){
 	return result;
 }
 
+// Calculer les overlaps d'une matrice
+vector< vector<int> > calculerOverlapM(vector<string> T, vector< vector<int> > M, int nbMots){
+	for(int i=0; i<nbMots; i++){
+		for(int j=0; j<nbMots; j++){
+			if(i==j){
+				M[i][j] = -1;
+			}
+			else{
+				M[i][j] = calculerOverlap(T[i],T[j]);
+			}
+		}
+	}
+	return M;
+}
+
+// Calculer l'overlap max
+int overlapMax(vector< vector<int> > M, int nbMots, int *vali, int *valj){
+	int max=-1;
+	for(int i=0; i<nbMots; i++){
+		for(int j=0; j<nbMots; j++){
+			if(M[i][j]>max){
+				max=M[i][j];
+				*vali=i;
+				*valj=j;
+			}
+		}
+	}
+	return max;
+}
+
+// Afficher Matrice
+void afficherMatrice(vector<string> T, vector< vector<int> > M, int nbMots, int ecart, int overlapMax, int x, int y){
+	bool maxTrouve=false;
+	for(int i=-1; i<nbMots; i++){
+		for(int j=-1; j<nbMots; j++){
+			if(i==-1 && j==-1){
+				cout<<setw(ecart)<<" ";
+			}
+			else if(i==-1){
+				if(j==y){
+					string mot = T[j];
+					int taille = mot.size();
+					cout<<setw(ecart)<<"\033[1;34m"<<mot.substr(0,overlapMax)<<"\033[0m"<<mot.substr(overlapMax,taille);
+				}
+				else{
+					cout<<setw(ecart)<<T[j];
+				}
+			}
+			else if(j==-1){
+				if(i==x){
+					string mot = T[i];
+					int taille = mot.size();
+					cout<<setw(ecart-overlapMax)<<mot.substr(0,taille-overlapMax)<<"\033[1;34m"<<mot.substr(taille-overlapMax,taille)<<"\033[0m";
+				}
+				else{
+					cout<<setw(ecart)<<T[i];
+				}
+			}
+			else{
+				if(!maxTrouve && M[i][j]==overlapMax){
+					cout<<"\033[1;31m"<<setw(ecart)<<M[i][j]<<"\033[0m";
+					maxTrouve=true;
+				}
+				else{
+					cout<<setw(ecart)<<M[i][j];
+				}
+			}
+		}
+		cout<<endl;
+	}
+}
+
 // Fusionner des overlaps
 string fusionOverlap(int i, int j, int overlapMaximum, vector<string> T){
 	string x = T[i];
@@ -91,40 +197,46 @@ int main(int argc, char *argv[]){
 		// Afficher la liste des mots donnés en entrée
 		cout<<endl;
 		vector<string> T = afficher_mots(nbMots, argv);
-		
-		string resultat ="";
-		int max = -1;
-		int maxIndiceI = -1;
-		int maxIndiceJ = -1;
-		int buf = -1;
-		while(T.size()>1){
-			for(int j=0; j<T.size()-1; j++){
-				buf = calculerOverlap(T[T.size()-1],T[j]);
-				if(buf>max){
-					max = buf;
-					maxIndiceI = T.size()-1;
-					maxIndiceJ = j;
-				}
-				buf = calculerOverlap(T[j],T[T.size()-1]);
-				if(buf>max){
-					max = buf;
-					maxIndiceI = j;
-					maxIndiceJ = T.size()-1;
-				}
-			}
-			
-			T.push_back(fusionOverlap(maxIndiceI, maxIndiceJ, max, T));
-			T.erase(T.begin()+ maxIndiceI);
-			T.erase(T.begin()+ maxIndiceJ);
-			max = -1;
-			maxIndiceI = -1;
-			maxIndiceJ = -1;
-			buf = -1;
-		}	
-		resultat = T[0];
-		
+		string motFusion = "";
+		cout<<endl;
 
-		cout<<"Super-chaîne : "<<resultat<<" (Taille "<<resultat.size()<<")"<<endl<<endl;
+		// Vérifier si certains mots sont inclus dans d'autres
+		T = suppressionMotsInclus(nbMots,T);
+		while(nbMots>1){
+
+			int ecartAffichage = longueurMax(nbMots,T)+3;
+
+			// Parcourir tous les mots, calculer et afficher les overlaps
+			vector< vector<int> > M(nbMots, vector<int>(nbMots));
+			M = calculerOverlapM(T,M,nbMots);
+			int i,j;
+			int overlapMaximum = overlapMax(M,nbMots,&i,&j);
+			afficherMatrice(T,M,nbMots,ecartAffichage,overlapMaximum,i,j);
+
+			// Fusionner les overlaps et ajouter le mot fusionné
+			cout<<endl;
+			motFusion = fusionOverlap(i,j,overlapMaximum,T);
+			cout<<endl;
+			
+			if(i>j){
+				T.erase(T.begin()+ i);
+				M.erase(M.begin()+ i);
+				T.erase(T.begin()+ j);
+				M.erase(M.begin()+ j);
+				
+			}
+			else{
+				T.erase(T.begin()+ j);
+				M.erase(M.begin()+ j);
+				T.erase(T.begin()+ i);
+				M.erase(M.begin()+ i);
+			}
+			T.push_back(motFusion);
+			nbMots--;
+			
+		}
+
+		cout<<"Super-chaîne : "<<motFusion<<" (Taille "<<motFusion.size()<<")"<<endl<<endl;
 	}
 	return 0;
 }
